e83d3db92165eac450fe3dac00cdc4c5917797be
sktippanbone <sktippanbone@gmail.com>
2024-08-08 12:38:45 +0530

    AddedPlay buttons

---
+++ spotify.js
@@ -0,0 +1,95 @@
+console.log("Let's write JavaScript");
+let currentSong = new Audio();
+let currentSongIndex = 0;
+let songs = [];
+
+function formatTime(seconds) {
+  seconds = Math.floor(seconds);
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = seconds % 60;
+  return `${minutes}:${remainingSeconds.toString().padStart(2, '0')}`;
+}
+
+async function getSongs() {
+  let a = await fetch("./songss");
+  let response = await a.text();
+  let div = document.createElement("div");
+  div.innerHTML = response;
+  let as = div.getElementsByTagName("a");
+  let songss = [];
+  for (let index = 0; index < as.length; index++) {
+    const element = as[index];
+    if (element.href.endsWith(".mp3")) {
+      songss.push(element.href);
+    }
+  }
+  return songss;
+}
+
+const playMusic = (track) => {
+  currentSong.src = track;
+  currentSong.play();
+  play.src = "pause.svg";
+  document.querySelector(".songinfo").innerHTML = track;
+  document.querySelector(".songtime").innerHTML = "00:00 / 00:00";
+}
+
+const playNextSong = () => {
+  currentSongIndex = (currentSongIndex + 1) % songs.length;
+  playMusic(songs[currentSongIndex]);
+}
+
+const playPreviousSong = () => {
+  currentSongIndex = (currentSongIndex - 1 + songs.length) % songs.length;
+  playMusic(songs[currentSongIndex]);
+}
+
+async function main() {
+  songs = await getSongs();
+  console.log(songs);
+
+  let songUL = document.querySelector(".lists").getElementsByTagName("ul")[0];
+  for (const song of songs) {
+    songUL.innerHTML += `
+      <li>
+        <img src="SVG/music.svg" class="invert">
+        <div class="info">
+          <div>${song.replaceAll("%20", " ")}</div>
+          <div> Artist Name</div>
+        </div>
+        <div class="playnow flex">
+          Play Now
+          <img src="SVG/play.svg" class="invert">
+        </div>
+      </li>`;
+  }
+
+  Array.from(document.querySelector(".lists").getElementsByTagName("li")).forEach((e, index) => {
+    e.addEventListener("click", () => {
+      console.log(e.querySelector(".info").firstElementChild.innerHTML);
+      currentSongIndex = index;
+      playMusic(songs[currentSongIndex]);
+    });
+  });
+
+  
+  document.getElementById("play").addEventListener("click", () => {
+    if (currentSong.paused) {
+      currentSong.play();
+      document.getElementById("play").src = "SVG/pause.svg";
+    } else {
+      currentSong.pause();
+      document.getElementById("play").src = "SVG/play.svg";
+    }
+  });
+
+  next.addEventListener("click", playNextSong);
+  previous.addEventListener("click", playPreviousSong);
+
+  currentSong.addEventListener("timeupdate", () => {
+    console.log(currentSong.currentTime, currentSong.duration);
+    document.querySelector(".songtime").innerHTML = `${formatTime(currentSong.currentTime)} / ${formatTime(currentSong.duration)}`;
+  });
+}
+
+main();
